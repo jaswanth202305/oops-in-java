a785f7cf208f83876aa4d1cfedb4220a6b489555
jaswanth202305 <jaswanthdpb@gmail.com>
2025-05-05 16:26:26 +0530

    commit 2

--- demo_git/src/demo/GitDemo.java
+++ demo_git/src/demo/GitDemo.java
@@ -7,7 +7,7 @@ public class GitDemo {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        System.out.println("Hi");
         System.out.println("Git demo");
 
     }
