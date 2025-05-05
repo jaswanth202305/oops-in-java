e155ac9dae6d28894846765e11ecb9f24d61579c
jaswanth202305 <jaswanthdpb@gmail.com>
2025-05-05 16:00:59 +0530

    first commit

---
+++ demo_git/src/demo/GitDemo.java
@@ -0,0 +1,16 @@
+/**
+ * Day 05-May-2025 of DSA Journey in Java Author: Jaswanth Date: 05-May-2025
+ */
+package demo;
+
+public class GitDemo {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("Git demo");
+
+    }
+
+}
+
